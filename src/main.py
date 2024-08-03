079c77761b87fbd82db4a56c689fb172cd5a01aa
wisetlaloc <13391342+wisetlaloc@users.noreply.github.com>
2024-08-03 23:22:17 +0900

    Add recursive page generation and new article
    Enhanced the static site generator to support recursive page generation, allowing for organized content directories. Also, added a detailed article on "The Lord of the Rings" to the content section.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from markdown import markdown_to_html_node, extract_title
 
 def main():
     copy_to_dest("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursively("content", "template.html", "public")
 
 
 def copy_to_dest(src_path, dest_path):
@@ -21,6 +21,17 @@ def copy_to_dest(src_path, dest_path):
             copy_to_dest(src_item, dest_item)
 
 
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+    for item in os.listdir(dir_path_content):
+        src_item = os.path.join(dir_path_content, item)
+        dest_item = os.path.join(dest_dir_path, item)
+        if os.path.isfile(src_item):
+            dest_item_html = os.path.splitext(dest_item)[0] + '.html'
+            generate_page(src_item, template_path, dest_item_html)
+        else:
+            generate_pages_recursively(src_item, template_path, dest_item)
+
+
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as from_file:
@@ -31,6 +42,7 @@ def generate_page(from_path, template_path, dest_path):
     html_string = html_node.to_html()
     title = extract_title(markdown)
     new_page = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as dest_file:
         dest_file.write(new_page)
 
